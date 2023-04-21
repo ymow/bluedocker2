9e75ab07a79698a850c18ac8c17dff56cd90dcc8
ymow <kenshin0812@hotmail.com>
2023-04-21 17:42:57 +0800

    http 500 issue

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    nitro: {
-        socket: '/tmp/nitro/myapp-worker.sock'
-    },
+    // nitro: {
+    //     socket: '/tmp/nitro/myapp-worker2.sock'
+    // },
     server: {
         port: 8080, // default: 3000
         host: '0.0.0.0', // default: localhost
+    },
+    vue: {
+        config: {
+            productionTip: false,
+            devtools: true,
+        },
     },
 })
