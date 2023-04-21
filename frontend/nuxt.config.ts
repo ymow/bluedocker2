fdd26deb9d5c771d462c3a0511dd5691c34f35ac
ymow <kenshin0812@hotmail.com>
2023-04-21 17:48:45 +0800

    add nitro worker3 back

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // nitro: {
-    //     socket: '/tmp/nitro/myapp-worker2.sock'
-    // },
+    nitro: {
+        socket: '/tmp/nitro/myapp-worker3.sock'
+    },
     server: {
         port: 8080, // default: 3000
         host: '0.0.0.0', // default: localhost
